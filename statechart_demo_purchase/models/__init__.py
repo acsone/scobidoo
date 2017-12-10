5b6814d76de34e5c7a8660b3336bb2883b4d0e0a
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-12-10 12:19:26 +0100

    add another inheritance test

--- statechart_demo_purchase/models/__init__.py
+++ statechart_demo_purchase/models/__init__.py
@@ -1,3 +1,4 @@
 from . import purchase_order
 from . import purchase_order_delegated
+from . import purchase_order_inherited
 from . import inherit
---
+++ statechart_demo_purchase/models/purchase_order_inherited.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class PurchaseOrderInherited(models.Model):
+
+    _inherit = 'purchase.order'
+    _name = 'purchase.order.inherited'
