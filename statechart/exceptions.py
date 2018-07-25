24b4f4afc5de15cc0bf454cf5db7758640b704c1
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-07-25 08:02:10 +0200

    copyright headers

--- statechart/exceptions.py
+++ statechart/exceptions.py
@@ -1,7 +1,7 @@
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.exceptions import UserError
+from odoo.exceptions import UserError
 
 
 class NoTransitionError(UserError):
