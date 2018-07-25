bf58898596544a0ffd3d3d97ed337518ce2bbded
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-07-25 08:04:36 +0200

    Copyright headers

--- statechart_demo_purchase/models/purchase_order.py
+++ statechart_demo_purchase/models/purchase_order.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 ACSONE SA/NV
+# Copyright 2016-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, _
