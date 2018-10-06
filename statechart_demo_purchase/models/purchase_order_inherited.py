8d4605dd0e9ae6d30e6d590ea7956c9ba22d72e8
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-10-06 13:10:14 +0200

    Use LGPL-3 license
    Since it is a low level library, it should make it more easy to adopt
    in more different circumstances.

--- statechart_demo_purchase/models/purchase_order_inherited.py
+++ statechart_demo_purchase/models/purchase_order_inherited.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2017 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from openerp import models
 
