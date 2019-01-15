c982c40a1210717a0393b36687d1a6de7d529e84
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-01-15 19:06:41 +0100

    [FIX] bug in _sc_patch when base class has no statechart mixin

--- statechart_demo_purchase/models/__init__.py
+++ statechart_demo_purchase/models/__init__.py
@@ -2,3 +2,4 @@ from . import purchase_order
 from . import purchase_order_delegated
 from . import purchase_order_inherited
 from . import inherit
+from . import thing_with_mailthread
---
+++ statechart_demo_purchase/models/thing_with_mailthread.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from openerp import models
+
+
+class ThingWithMailthread(models.Model):
+
+    _name = 'thing.with.mailthread'
+    _description = "Thing with mail thread"
+    _inherit = ['mail.thread', 'statechart.mixin']
+    _statechart_file = 'statechart_demo_purchase/models/simple_statechart.yml'
