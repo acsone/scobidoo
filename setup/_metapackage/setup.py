f37d7ad369cca21ac6ee8dc5e35e57a14003933b
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-10-21 20:40:06 +0200

    [ADD] setup.py

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -4,12 +4,12 @@ with open('VERSION.txt', 'r') as f:
     version = f.read().strip()
 
 setuptools.setup(
-    name="odoo12-addons-acsone-scobidoo",
-    description="Meta package for acsone-scobidoo Odoo addons",
+    name="odoo13-addons-odoo12-addons-acsone-scobidoo",
+    description="Meta package for odoo12-addons-acsone-scobidoo Odoo addons",
     version=version,
     install_requires=[
-        'odoo12-addon-statechart',
-        'odoo12-addon-statechart_demo_purchase',
+        'odoo13-addon-statechart',
+        'odoo13-addon-statechart_demo_purchase',
     ],
     classifiers=[
         'Programming Language :: Python',
