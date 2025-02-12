4ebf6be9b94fa7ceaa2c4812f0d6205d10c5003a
hda <hughes.damry@acsone.eu>
2025-02-12 12:03:36 +0100

    [18.0][MIG] statechart

--- statechart_demo/__manifest__.py
+++ statechart_demo/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Statechart Demo",
     "summary": """Demo workflows for the statechart module""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/acsone/scobidoo",
