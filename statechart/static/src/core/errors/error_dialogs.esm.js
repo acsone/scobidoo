3946696da75f183bc8449414afc275ee950e88f5
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2025-08-13 10:58:23 +0200

    [MIG] statechart 18.0: display error pop up instead of stacktrace when raising a TransitionError From https://github.com/acsone/scobidoo/pull/46

--- statechart/static/src/core/errors/error_dialogs.esm.js
+++ statechart/static/src/core/errors/error_dialogs.esm.js
@@ -1,13 +1,10 @@
-/** @odoo-module **/
-
-import {_lt} from "@web/core/l10n/translation";
-import {registry} from "@web/core/registry";
-
 import {WarningDialog, odooExceptionTitleMap} from "@web/core/errors/error_dialogs";
+import {_t} from "@web/core/l10n/translation";
+import {registry} from "@web/core/registry";
 
 registry
     .category("error_dialogs")
     .add("odoo.addons.statechart.exceptions.NoTransitionError", WarningDialog);
 
 odooExceptionTitleMap["odoo.addons.statechart.exceptions.NoTransitionError"] =
-    _lt("Transition Error");
+    _t("Transition Error");
