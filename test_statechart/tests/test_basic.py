4634de12efd091b3ebf6355e81a0abe1c9a76dbe
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-08-13 12:51:58 +0200

    [IMP] handle ignore_for_has_allowed_events only in the compute method
    There is no need to leak this context handling to
    _get_sc_event_allowed_field_names, where
    this can be handled with a new argument.

--- test_statechart/tests/test_basic.py
+++ test_statechart/tests/test_basic.py
@@ -38,7 +38,7 @@ class TestBasic(common.TransactionCase):
         # small amount, step 2 done automatically
         self.assertScState(record.sc_state, ["confirmed2", "root"])
 
-    def test_get_sc_event_allowed_field_names(self):
+    def test_get_sc_sc_has_allowed_events(self):
         """
         Test that we can ignore certain events based on a context
         key (ignore_for_has_allowed_events)
